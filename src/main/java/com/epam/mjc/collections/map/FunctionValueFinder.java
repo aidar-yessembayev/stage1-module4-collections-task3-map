5ced590ad9b878f4ee7fc028ba86496607517ed1
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-25 11:25:59 +0600

    Update FunctionValueFinder.java

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,37 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
-        Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        if (sourceList.size() > 0) {
+            Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+            for (Integer number : functionMap.keySet()) {
+                if (functionMap.get(number) == requiredValue) {
+                    return true;
+                }
+            }
+
+        }
+
+        return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < sourceList.size(); i++) {
+            if (!result.containsKey(sourceList.get(i))) {
+                int calculation = 5 * sourceList.get(i) + 2;
+                result.put(sourceList.get(i), calculation);
+            }
+        }
+
+        return result;
     }
 }
