c906a8d41e9e5a742e1354709918f0c33b70dd7f
aidar-yessembayev <98548293+aidar-yessembayev@users.noreply.github.com>
2023-01-25 11:26:19 +0600

    Update KeyValueSwapper.java

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,24 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+
+        if (sourceMap.size() > 0) {
+            for (Integer number : sourceMap.keySet()) {
+                String word = sourceMap.get(number);
+
+                if ((result.containsKey(word)) && (number > result.get(word))) {
+                    continue;
+                }
+
+                result.put(word, number);
+            }
+        }
+
+        return result;
     }
 }
